99317387c86f608c1381d8b3d30988f2f67db73f
ankush-manaktala <85988745+ankush-manaktala@users.noreply.github.com>
2022-12-30 12:32:59 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -9,6 +9,28 @@ function calculateVat(input1, input2) {
   sendData(data);
 }
 
+function GetSubTotal(basketProducts) {
+  let result = GetTotal(basketProducts, 'derivedPriceWithSubTotal');
+  sendData(result);
+}
+
+//Get total of specific key from an array. Note:- pass key in String format!
+function GetTotal(products, key) {
+  if (key) {
+    var total;
+    if (products.length > 0) {
+      total = products.reduce(
+        (prev, curr) =>
+          parseFloat(prev) + parseFloat(curr[key] ? curr[key] : 0),
+        0,
+      );
+    }
+    return total;
+  } else {
+    return 0;
+  }
+}
+
 function sendData(data) {
   window.ReactNativeWebView.postMessage(JSON.stringify(data));
 }
