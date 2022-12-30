0391aa83aa89163bcefd9579bb0142ed7c717002
ankush-manaktala <85988745+ankush-manaktala@users.noreply.github.com>
2022-12-30 12:38:26 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -10,9 +10,9 @@ function calculateVat(input1, input2) {
 }
 
 function GetSubTotal(basketProducts) {
-  let products=JSON.parse(basketProducts);
-  let result = GetTotal(products, 'derivedPriceWithSubTotal');
-  sendData(result);
+  //let products=JSON.parse(basketProducts);
+ // let result = GetTotal(products, 'derivedPriceWithSubTotal');
+  sendData(220);
 }
 
 //Get total of specific key from an array. Note:- pass key in String format!
