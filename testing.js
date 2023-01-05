bbf1a817286b446236ba2354d27f8e3c41c01c96
ankush-manaktala <85988745+ankush-manaktala@users.noreply.github.com>
2023-01-05 19:31:23 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -9,82 +9,82 @@ function calculateVat(input1, input2) {
   sendData(data);
 }
 
-function GetSubTotal(basketProducts,products) {
-  const subTotal = GetTotal(basketProducts, 'derivedPriceWithSubTotal');
-  const totalTax = GetTotal(basketProducts, 'derivedTax');
-  const promoDiscount = GetTotalPromoDiscount(basketProducts);
-  const freeProductsPromo=GetTotalFreePromoDiscount(products?.freeProducts);
-  let contractualDiscount = GetTotal(basketProducts, "contractualDiscount");
-  let result={subTotal,
-              totalTax,
-              contractualDiscount,
-              totalPromoDiscount:promoDiscount + freeProductsPromo }
-  sendData(result);
-}
+//function GetSubTotal(basketProducts,products) {
+//   const subTotal = GetTotal(basketProducts, 'derivedPriceWithSubTotal');
+//   const totalTax = GetTotal(basketProducts, 'derivedTax');
+//   const promoDiscount = GetTotalPromoDiscount(basketProducts);
+//   const freeProductsPromo=GetTotalFreePromoDiscount(products?.freeProducts);
+//   let contractualDiscount = GetTotal(basketProducts, "contractualDiscount");
+//   let result={subTotal,
+//               totalTax,
+//               contractualDiscount,
+//               totalPromoDiscount:promoDiscount + freeProductsPromo }
+//   sendData(result);
+// }
 
-//Get total of specific key from an array. Note:- pass key in String format!
-function GetTotal(products, key) {
-  if (key) {
-    var total;
-    if (products.length > 0) {
-      total = products.reduce(
-        (prev, curr) =>
-          parseFloat(prev) + parseFloat(curr[key] ? curr[key] : 0),
-        0,
-      );
-    }
-    return total;
-  } else {
-    return 0;
-  }
-}
+// //Get total of specific key from an array. Note:- pass key in String format!
+// function GetTotal(products, key) {
+//   if (key) {
+//     var total;
+//     if (products.length > 0) {
+//       total = products.reduce(
+//         (prev, curr) =>
+//           parseFloat(prev) + parseFloat(curr[key] ? curr[key] : 0),
+//         0,
+//       );
+//     }
+//     return total;
+//   } else {
+//     return 0;
+//   }
+// }
 
-//Get total promo discounts on all the products in cart.
-function GetTotalPromoDiscount(basketProducts){
-  var total = 0;
-  if (basketProducts.length > 0) {
-    const fixed = basketProducts.reduce(
-      (prev, curr) =>
-        parseFloat(prev) + parseFloat(curr?.derivedPromoDiscount?.fixed),
-      0
-    );
-    const buyXGetY = basketProducts.reduce(
-      (prev, curr) =>
-        parseFloat(prev) + parseFloat(curr?.derivedPromoDiscount?.buyXGetY),
-      0
-    );
+// //Get total promo discounts on all the products in cart.
+// function GetTotalPromoDiscount(basketProducts){
+//   var total = 0;
+//   if (basketProducts.length > 0) {
+//     const fixed = basketProducts.reduce(
+//       (prev, curr) =>
+//         parseFloat(prev) + parseFloat(curr?.derivedPromoDiscount?.fixed),
+//       0
+//     );
+//     const buyXGetY = basketProducts.reduce(
+//       (prev, curr) =>
+//         parseFloat(prev) + parseFloat(curr?.derivedPromoDiscount?.buyXGetY),
+//       0
+//     );
 
-    total = fixed + buyXGetY;
-  }
-  return total;
-};
+//     total = fixed + buyXGetY;
+//   }
+//   return total;
+// };
 
-export const GetTotalFreePromoDiscount = (freeProducts) => {
-  let freeProductsTotal = 0;
-  if (freeProducts?.length > 0) {
-    freeProductsTotal = freeProducts.reduce(
-      (prev, curr) =>
-        parseFloat(prev) +
-        parseFloat(curr?.product?.price?.basePrice * curr?.freeProductQty),
-      0
-    );
-  }
-  return freeProductsTotal;
-};
+// export const GetTotalFreePromoDiscount = (freeProducts) => {
+//   let freeProductsTotal = 0;
+//   if (freeProducts?.length > 0) {
+//     freeProductsTotal = freeProducts.reduce(
+//       (prev, curr) =>
+//         parseFloat(prev) +
+//         parseFloat(curr?.product?.price?.basePrice * curr?.freeProductQty),
+//       0
+//     );
+//   }
+//   return freeProductsTotal;
+// };
 
-//Get total of specific key from an array. Note:- pass key in String format!
-function GetTotalValue(products, key) {
-  let result=0;
-  if (key && products?.length > 0) {
-      result = products.reduce(
-        (prev, curr) =>
-          parseFloat(prev) + parseFloat(curr[key] ? curr[key] : 0),
-        0,
-      );
-  }
-  sendData(result);
-}
-
+// //Get total of specific key from an array. Note:- pass key in String format!
+// function GetTotalValue(products, key) {
+//   let result=0;
+//   if (key && products?.length > 0) {
+//       result = products.reduce(
+//         (prev, curr) =>
+//           parseFloat(prev) + parseFloat(curr[key] ? curr[key] : 0),
+//         0,
+//       );
+//   }
+//   sendData(result);
+// }
+  
 function sendData(data) {
   window.ReactNativeWebView.postMessage(JSON.stringify(data));
 }
